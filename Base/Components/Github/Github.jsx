aa476b38ddd91eeb55874b5ba6f3100c450967d4
MoushufAlam <rishu78698@gmail.com>
2025-02-09 01:53:34 +0530

    Project Completed

---
+++ Base/Components/Github/Github.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+
+function Github() {
+    const [data,setData] = useState([]);
+    useEffect(()=>{
+        fetch('https://api.github.com/users/MoushufAlam')
+        .then((response) => response.json())
+        .then((data) => {
+            setData(data)
+        })
+    },[])
+    return (
+        <>
+        <div className='text-center bg-gray-600 m-4 p-4 text-3xl text-white'>Github followers: {data.followers}
+        <img className='' src={data.avatar_url} alt="Profile Pic" width={300} /></div>
+        </>
+    )
+}
+
+export default Github
